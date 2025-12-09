0d34ed08a42e53a944f50b35202ae8786c1ea347
dsokol101 <dsokol@brooklyn.cuny.edu>
2025-12-09 15:28:29 -0500

    Refactor app.cpp and pair.h to enhance clarity and add comments on std::pair and map usage

--- app.cpp
+++ app.cpp
@@ -5,23 +5,27 @@
 #include <iostream> // std::cout
 #include <map>      // std::map
 #include <vector>   // std::vector
+
 using namespace std;
 
 int main()
 {
+    // uses std::pair
     pair<string, double> product1;                   // default constructor
     pair<string, double> product2("tomatoes", 2.30); // value init
     pair<string, double> product3(product2);         // copy constructor
 
-    product1 = make_pair(string("lightbulbs"), 0.99); // using make_pair (move)
-
+    product1 = make_pair(string("lightbulbs"), 0.99); // using make_pair (move) 
     product2.first = "shoes"; // the type of first is string
     product2.second = 39.90;  // the type of second is double
 
     cout << "The price of " << product1.first << " is $" << product1.second << '\n';
     cout << "The price of " << product2.first << " is $" << product2.second << '\n';
     cout << "The price of " << product3.first << " is $" << product3.second << '\n';
-    /************************************* */
+    /************************************* 
+     * introduction to map data structure in C++ STL
+     * A map is a sorted associative container that contains key-value pairs with unique keys.
+    */
     map<string, vector<int>> mymap;
     mymap["Alice"] = {90, 87, 95};
     mymap["Bob"] = {85, 80, 88};
--- pair.h
+++ pair.h
@@ -1,6 +1,10 @@
-/** 5/13/2025 */
+#pragma once
+#include <iostream>
+#include <utility>
 /** A class template is a class that receives one or more datatypes
  * as parameters to the class. It's really a blueprint for creating classes.
+ *
+ * A struct is like a class, except that its members are public by default.
  */
 template <typename T1, typename T2>
 struct pair
@@ -12,10 +16,16 @@ struct pair
     pair(const T1 &a, const T2 &b) : first(a), second(b) {}
     pair(const pair &p) : first(p.first), second(p.second) {}
     pair(pair &&p) : first(std::move(p.first)), second(std::move(p.second)) {}
-   // template <class T1, class T2>
-    pair<T1, T2> make_pair(T1 x, T2 y)
-    {
-        return (pair<T1, T2>(x, y)); // constructor called to create a pair object
-        // then copy constructor is called to copy the object to the return value
-    }
 };
+// this is called a factory function
+template <typename T1, typename T2>
+pair<T1, T2> make_pair(const T1 &x, const T2 &y)
+{
+    return pair<T1, T2>(x, y); // constructor called to create a pair object
+                               // then copy (or move) constructor is called to copy the object to the return value
+}
+template <typename T1, typename T2>
+void print_pair(const pair<T1, T2> &p)
+{
+    std::cout << "(" << p.first << ", " << p.second << ")" << std::endl;
+}
